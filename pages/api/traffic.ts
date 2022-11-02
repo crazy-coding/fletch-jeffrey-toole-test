1c7ed88d329f3b719aeff2d76a7786728142a099
crazy-coding <dennischo122@gmail.com>
2022-11-02 18:36:11 -0400

    fix: debug server issue

--- pages/api/traffic.ts
+++ pages/api/traffic.ts
@@ -2,8 +2,11 @@ import path from 'path';
 import { promises as fs } from 'fs';
 
 export default async function handler(req: any, res: any) {
-  console.log(process.cwd())
-  const jsonDirectory = path.join(process.cwd(), 'public/json');
-  const fileContents = await fs.readFile(jsonDirectory + '/traffic.json', 'utf8');
-  res.status(200).json(fileContents);
+  try {
+    const jsonDirectory = path.join(process.cwd(), 'json');
+    const fileContents = await fs.readFile(jsonDirectory + '/traffic.json', 'utf8');
+    res.status(200).json(fileContents);
+  } catch (e) {
+    res.status(500).json(e)
+  }
 }
