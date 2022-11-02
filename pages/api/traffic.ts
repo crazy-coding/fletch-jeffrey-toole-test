441ae45a755e7e1ee3a8adea57e7456709c545ad
crazy-coding <dennischo122@gmail.com>
2022-11-02 18:44:42 -0400

    fix: restore server api

--- pages/api/traffic.ts
+++ pages/api/traffic.ts
@@ -2,11 +2,7 @@ import path from 'path';
 import { promises as fs } from 'fs';
 
 export default async function handler(req: any, res: any) {
-  try {
-    const jsonDirectory = path.join(process.cwd(), 'json');
-    const fileContents = await fs.readFile(jsonDirectory + '/traffic.json', 'utf8');
-    res.status(200).json(fileContents);
-  } catch (e) {
-    res.status(500).json(e)
-  }
+  const jsonDirectory = path.join(process.cwd(), 'json');
+  const fileContents = await fs.readFile(jsonDirectory + '/traffic.json', 'utf8');
+  res.status(200).json(fileContents);
 }
